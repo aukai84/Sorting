eeaadb78203995440af382b7399cb473a60675ed
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-02 21:47:07 -1000

    cleaned up some stuff

--- routes/bubble-sort.js
+++ routes/bubble-sort.js
@@ -2,20 +2,17 @@ const express = require('express');
 const router = express('router');
 const sortModule = require('../modules/bubble-sort-module.js');
 const bubbleSort = sortModule.bubbleSort;
+
 router.get('/', (req,res) => {
     res.render('index');
 });
 
 router.post('/', (req,res) => {
-    console.log(req.body);
     let unsortedArray = [];
     for(let i in req.body){
         unsortedArray.push(parseInt(req.body[i]));
     }
-    console.log(unsortedArray)
     let sortedArray = bubbleSort(unsortedArray);
-    console.log(sortedArray);
-
     res.render('index', {array: sortedArray});
 });
 
