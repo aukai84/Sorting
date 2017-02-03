c13bcc87da70bf734d7c2681b536933b7309a1b8
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-02 19:08:22 -1000

    post route sorting array from web page..need to display sorted array

--- routes/bubble-sort.js
+++ routes/bubble-sort.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express('router');
-const bubbleSort = require('../modules/bubble-sort-module.js');
-
+const sortModule = require('../modules/bubble-sort-module.js');
+const bubbleSort = sortModule.bubbleSort;
 router.get('/', (req,res) => {
     res.render('index');
 });
 
 router.post('/', (req,res) => {
-    console.log(req.body)
+    console.log(req.body);
     let unsortedArray = [];
-    for(let key in req.body){
-        unsortedArray.push(parseInt(req.body.key));
+    for(let i in req.body){
+        unsortedArray.push(parseInt(req.body[i]));
     }
-    console.log(unsortedArray);
+    let sortedArray = bubbleSort(unsortedArray);
+    console.log(sortedArray);
+
 });
 
 module.exports = router;
