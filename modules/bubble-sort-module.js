a1da8ea91a34318551b996a5c7596c0d8648ad54
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-02 17:44:23 -1000

    updated function to use do while loop

--- modules/bubble-sort-module.js
+++ modules/bubble-sort-module.js
@@ -1,15 +1,19 @@
 
 function bubbleSort(array) {
-    //sort array here
-    for(let i = 0; i < array.length; i ++){
-        if(array[i] > array[i + 1]){
-            let temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
-        }
-    }
-    return array;
+    let swapped;
 
+    do {
+        swapped = false;
+        for(let i = 0; i < array.length; i ++){
+            if(array[i] > array[i + 1]){
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return array;
 }
 let unsortedArray = [5,1,2,4,8,88,7,10];
 
