d13eea0937dd545f1f9f6e4a75629df921fe0bb7
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-04 21:58:34 -1000

    added merge sort module working with server

--- modules/merge-sort-module.js
+++ modules/merge-sort-module.js
@@ -1,13 +1,39 @@
 function mergeSort(array) {
-    // if(array.length <= 1){
-    //     return array;
-    // }
-    // let leftArray = [];
-    // let rightArray = [];
-    // for(let i = 0; i < array.length; i++){
-    //     if()
-    // }
+    if(array.length <= 1){
+        return array;
+    }
 
+    let middle = parseInt(Math.floor(array.length/2));
+    let leftArray = array.slice(0, middle);
+    let rightArray = array.slice(middle, array.length);
+    console.log("left array = ", leftArray);
+    console.log("right array = ", rightArray);
+    let sortedArray = merge(mergeSort(leftArray), mergeSort(rightArray));
+    return sortedArray;
+}
+
+function merge(left, right) {
+    let result = [];
+    while(left.length && right.length){
+        if(left[0] > right[0]){
+            result.push(right[0]);
+            right.shift();
+        } else {
+            result.push(left[0]);
+            left.shift();
+        }
+    }
+    while(right.length){
+        result.push(right[0]);
+        right.shift();
+    }
+
+    while(left.length){
+        result.push(left[0]);
+        left.shift();
+    }
+    console.log(result);
+    return result;
 }
 
 module.exports = mergeSort;
--- routes/merge-sort.js
+++ routes/merge-sort.js
@@ -2,5 +2,14 @@ const express = require('express');
 const router = express('router');
 const mergeSort = require('../modules/merge-sort-module.js');
 
+router.post('/', (req, res) => {
+    console.log('merge sorting....');
+    let unsortedArray = [];
+    for(let i in req.body){
+        unsortedArray.push(parseInt(req.body[i]));
+    }
+    let sortedArray = mergeSort(unsortedArray);
+    res.render('index', {array: sortedArray, name: "Merge Sort"});
+});
 
 module.exports = router;
